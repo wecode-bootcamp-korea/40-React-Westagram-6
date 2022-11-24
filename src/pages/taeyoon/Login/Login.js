e5eeec718136d14a3e389e4695753123cb37b9ac
KimTaeYoon <83495141+eric568@users.noreply.github.com>
2022-11-24 19:54:18 +0900

    Add : Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/taeyoon/Login/Login.js
+++ src/pages/taeyoon/Login/Login.js
@@ -4,35 +4,55 @@ import { useNavigate } from "react-router-dom";
 
 const LoginForm = () => {
   const navigate = useNavigate();
-  let [inputIdValue, inputIdFunc] = useState("");
-  let [inputPwValue, inputPwFunc] = useState("");
+  // let [inputIdValue, inputIdFunc] = useState("");
+  // let [inputPwValue, inputPwFunc] = useState("");
+  let [inputValues, setInput] = useState({
+    id: "",
+    pw: "",
+  });
+  const { id, pw } = inputValues;
+
+  const isId = id.includes("@");
+  const isPw = pw.length >= 5;
+  const isIdPw = isId && isPw;
+  const isdisabled = isIdPw ? false : true;
+
+  const checkInput = e => {
+    // console.log("2", inputValues);
+    setInput({
+      ...inputValues, //[id, pw]
+      [e.target.name]: e.target.value,
+    });
+    // console.log("3", inputValues);
+  };
 
   return (
     <form className="login__form">
       <input
-        onChange={e => inputIdFunc(e.target.value)}
+        onChange={checkInput}
         className="login__form--input input__id"
+        name="id"
+        value={id}
         type="text"
         placeholder="전화번호, 사용자 이름 또는 이메일"
         required
       />
-      {inputIdValue}
       <input
-        onChange={e => inputPwFunc(e.target.value)}
+        onChange={checkInput}
         className="login__form--input input__pw"
+        name="pw"
+        value={pw}
         type="password"
         placeholder="비밀번호"
         required
       />
-      {inputPwValue}
-
       <button
         onClick={() => {
           navigate("/main-taeyoon");
         }}
         className="login__form--btn"
         type="submit"
-        // disabled
+        disabled={isdisabled}
       >
         로그인
       </button>
