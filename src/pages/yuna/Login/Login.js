3f7f0b0daa1783b49725098ffba503543ce3ee5b
yunakim <dbssla31@gmail.com>
2022-12-02 14:25:06 +0900

    fetch적용

--- src/pages/yuna/Login/Login.js
+++ src/pages/yuna/Login/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 //useState 선언
-import { Link } from "react-router-dom";
 import "../Login/Login.scss";
 
 const Login = () => {
@@ -10,7 +10,7 @@ const Login = () => {
   //setId 값은 첫 번째 요소인 상태값을 업데이트 하는 함수
   //배열 안에 두개의 변수를 선언하고, useState를 호출한 뒤 반환값을 할당
   //useState 선언법
-
+  const navigate = useNavigate();
   const goMain = id.includes("@") && pw.length >= 5;
   //if문을 3항 연산자로 변환한 것
   //.includes = array에 주어진 요소가 있는지 확인하는 메서드
@@ -36,12 +36,56 @@ const Login = () => {
           type="password"
           placeholder="비밀번호"
         />
-        <Link to="/main-yuna">
-          <button class={goMain ? "loginBtn2" : "loginBtn"} disabled={goMain2}>
-            {/* goMain(id input값에 @포함하고 pw input값의 길이가 5 이상이면) disabled(버튼비활성화) 를 goMain2(goMain이 true면 비활성화를 false하고 false면 비활성화를  true하라) */}
-            로그인
-          </button>
-        </Link>
+
+        <button
+          onClick={() => {
+            fetch("http://10.58.52.240:8000/auth/signin", {
+              method: "POST",
+              headers: { "content-Type": "application/json;charset=utf-8" },
+              body: JSON.stringify({
+                email: id,
+                password: pw,
+              }),
+            })
+              .then(res => res.json())
+              .then(data => {
+                if (data.accessToken) {
+                  localStorage.setItem("token", data.accessToken);
+
+                  alert("환영합니다!");
+                  navigate("/main-yuna");
+                } else if (data.message === "specified user does not exist") {
+                  alert("존재하지 않는 유저입니다.");
+                }
+              });
+          }}
+          class={goMain ? "loginBtn2" : "loginBtn"}
+          disabled={goMain2}
+        >
+          {/* goMain(id input값에 @포함하고 pw input값의 길이가 5 이상이면) disabled(버튼비활성화) 를 goMain2(goMain이 true면 비활성화를 false하고 false면 비활성화를  true하라) */}
+          로그인
+        </button>
+
+        <button
+          onClick={() => {
+            fetch("http://10.58.52.240:8000/auth/signup", {
+              method: "POST",
+              headers: { "content-Type": "application/json;charset=utf-8" },
+              body: JSON.stringify({
+                email: id,
+                password: pw,
+              }),
+            });
+
+            // navigate("/main-yuna");
+          }}
+          class={goMain ? "loginBtn2" : "loginBtn"}
+          disabled={goMain2}
+        >
+          {/* goMain(id input값에 @포함하고 pw input값의 길이가 5 이상이면) disabled(버튼비활성화) 를 goMain2(goMain이 true면 비활성화를 false하고 false면 비활성화를  true하라) */}
+          회원가입
+        </button>
+
         <a className="pwLost" href="www.naver.com">
           비밀번호를 잊으셨나요??
         </a>
--- src/pages/yuna/Main/Main.js
+++ src/pages/yuna/Main/Main.js
@@ -1,9 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import CommentList from "./CommentList";
 import "../Main/Main.scss";
+import { FOOTER_LIST } from "./Footer";
 
 const Main = () => {
+  const [userInfoList, setUserInfoList] = useState([]);
+  console.log(userInfoList);
+  useEffect(() => {
+    fetch("/data/feedData.json")
+      .then(response => response.json())
+      .then(result => setUserInfoList(result));
+  }, []);
+
   let [userName] = useState("y_na__y");
   let [comment, setComment] = useState("");
   let [feedComments, setFeedComments] = useState([]);
@@ -65,127 +74,141 @@ const Main = () => {
           </div>
         </div>
       </div>
-      <div className="main">
-        <div className="article">
-          <div className="artHeader">
-            <div className="artHeaderProfile">
-              <img
-                className="artHeaderImg"
-                src="images/yuna/93D63A2A-9DE0-49B1-9FE3-AAD17B97D292.JPG"
-                alt="게시물프로필"
-              />
-              <span className="artHeaderId">long_yuna_</span>
-            </div>
 
-            <img
-              className="artHeaderIcon"
-              src="https://cdn-icons-png.flaticon.com/512/512/512142.png"
-              alt="더보기아이콘"
-            />
-          </div>
-          <div className="artMain">
-            <img
-              className="artMainImg"
-              src="images/yuna/19F50ED5-5118-471A-81AF-1E4F04E87C44.JPG"
-              alt="게시물이미지"
-            />
-          </div>
-          <div className="artFooter">
-            <div className="artFooterIcons">
-              <div className="artFooterFirstIcon">
+      <div className="main">
+        <div className="mainMainRight">
+          <div className="article">
+            <div className="artHeader">
+              <div className="artHeaderProfile">
                 <img
-                  className="artFooterIcon"
-                  src="https://cdn-icons-png.flaticon.com/512/1077/1077035.png"
-                  alt="좋아요이미지"
+                  className="artHeaderImg"
+                  src="images/yuna/93D63A2A-9DE0-49B1-9FE3-AAD17B97D292.JPG"
+                  alt="게시물프로필"
                 />
+                <span className="artHeaderId">long_yuna_</span>
+              </div>
+
+              <img
+                className="artHeaderIcon"
+                src="https://cdn-icons-png.flaticon.com/512/512/512142.png"
+                alt="더보기아이콘"
+              />
+            </div>
+            <div className="artMain">
+              <img
+                className="artMainImg"
+                src="images/yuna/19F50ED5-5118-471A-81AF-1E4F04E87C44.JPG"
+                alt="게시물이미지"
+              />
+            </div>
+            <div className="artFooter">
+              <div className="artFooterIcons">
+                <div className="artFooterFirstIcon">
+                  <img
+                    className="artFooterIcon"
+                    src="https://cdn-icons-png.flaticon.com/512/1077/1077035.png"
+                    alt="좋아요이미지"
+                  />
+                  <img
+                    className="artFooterIcon"
+                    src="https://cdn-icons-png.flaticon.com/512/3114/3114810.png"
+                    alt="말풍선이미지"
+                  />
+                  <img
+                    className="artFooterIcon"
+                    src="https://cdn-icons-png.flaticon.com/512/3097/3097412.png"
+                    alt="업로드이미지"
+                  />
+                </div>
                 <img
                   className="artFooterIcon"
-                  src="https://cdn-icons-png.flaticon.com/512/3114/3114810.png"
-                  alt="말풍선이미지"
-                />
-                <img
-                  className="artFooterIcon"
-                  src="https://cdn-icons-png.flaticon.com/512/3097/3097412.png"
-                  alt="업로드이미지"
+                  src="https://cdn-icons-png.flaticon.com/512/25/25667.png"
+                  alt="북마크이미지"
                 />
               </div>
-              <img
-                className="artFooterIcon"
-                src="https://cdn-icons-png.flaticon.com/512/25/25667.png"
-                alt="북마크이미지"
-              />
-            </div>
-            <div className="artFooterGood">
-              <img
-                className="artFooterGoodImg"
-                src="https://images.velog.io/images/hojin11choi/post/8af0e909-801c-4524-a62c-b83d02811ef7/wecode.png"
-                alt="위코드이미지"
-              />
-              <span>
-                <span className="artFooterPostId">Wecode</span>님{" "}
-                <span className="artFooterPostId">외 38,499명</span>이
-                좋아합니다
-              </span>
-            </div>
-            <div className="artFooterPost">
-              <span className="artFooterPostId">long_yuna_</span>
-              <span className="artFooterPostMyPost">무계획 오히려 좋아💙</span>
-            </div>
-
-            <ul className="artFooterComment">
-              <li className="artFooterCommentId">
-                <span>
-                  <span className="artFooterPostId">i_am_beetna</span> 뭐야?!
-                  나는!!
-                </span>
+              <div className="artFooterGood">
                 <img
-                  className="artFooterCommentIcoon"
-                  src="https://cdn-icons-png.flaticon.com/512/1077/1077035.png"
-                  alt="좋아요이미지"
+                  className="artFooterGoodImg"
+                  src="https://images.velog.io/images/hojin11choi/post/8af0e909-801c-4524-a62c-b83d02811ef7/wecode.png"
+                  alt="위코드이미지"
                 />
-              </li>
-            </ul>
+                <span>
+                  <span className="artFooterPostId">Wecode</span>님{" "}
+                  <span className="artFooterPostId">외 38,499명</span>이
+                  좋아합니다
+                </span>
+              </div>
+              <div className="artFooterPost">
+                <span className="artFooterPostId">long_yuna_</span>
+                <span className="artFooterPostMyPost">
+                  무계획 오히려 좋아💙
+                </span>
+              </div>
 
-            <span className="artFooterCommentTime">55분 전</span>
+              <ul className="artFooterComment">
+                <li className="artFooterCommentId">
+                  <span>
+                    <span className="artFooterPostId">i_am_beetna</span> 뭐야?!
+                    나는!!
+                  </span>
+                  <img
+                    className="artFooterCommentIcoon"
+                    src="https://cdn-icons-png.flaticon.com/512/1077/1077035.png"
+                    alt="좋아요이미지"
+                  />
+                </li>
+              </ul>
 
-            {feedComments.map((commentArr, i) => {
-              return (
-                <CommentList
-                  userName={userName}
-                  userComment={commentArr}
-                  key={i}
+              <span className="artFooterCommentTime">55분 전</span>
+
+              {feedComments.map((commentArr, i) => {
+                return (
+                  <CommentList
+                    userName={userName}
+                    userComment={commentArr}
+                    key={i}
+                  />
+                );
+              })}
+
+              <div className="artFooterCommentButton">
+                <input
+                  className="artFooterCommentText"
+                  type="text"
+                  placeholder="댓글달기..."
+                  onChange={e => {
+                    setComment(e.target.value);
+                  }}
+                  onKeyUp={e => {
+                    e.target.value.length > 0
+                      ? setIsValid(true)
+                      : setIsValid(false);
+                  }}
+                  value={comment}
+                  onKeyPress={handleOnKeyPress}
                 />
-              );
-            })}
-
-            <div className="artFooterCommentButton">
-              <input
-                className="artFooterCommentText"
-                type="text"
-                placeholder="댓글달기..."
-                onChange={e => {
-                  setComment(e.target.value);
-                }}
-                onKeyUp={e => {
-                  e.target.value.length > 0
-                    ? setIsValid(true)
-                    : setIsValid(false);
-                }}
-                value={comment}
-                onKeyPress={handleOnKeyPress}
-              />
-              <button
-                className={
-                  comment.length > 0
-                    ? "artfooterCommentBtn2"
-                    : "artfooterCommentBtn1"
-                }
-                onClick={post}
-                disabled={isValid ? false : true}
-              >
-                게시
-              </button>
+                <button
+                  className={
+                    comment.length > 0
+                      ? "artfooterCommentBtn2"
+                      : "artfooterCommentBtn1"
+                  }
+                  onClick={post}
+                  disabled={isValid ? false : true}
+                >
+                  게시
+                </button>
+              </div>
             </div>
+          </div>
+        </div>
+        <div className="main-right">
+          <div className="main-right-main">
+            <ul className="main-right-footer">
+              {FOOTER_LIST.map(footerList => {
+                return <li key={footerList.id}>{footerList.footerName}</li>;
+              })}
+            </ul>
           </div>
         </div>
       </div>
